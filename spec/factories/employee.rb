064efb9b71d6f108400cd772eeca2ae93401e300
MonikaLukowska <monika.lukowska@gmail.com>
2022-04-12 22:02:11 +0200

    fix employee factory

--- spec/factories/employee.rb
+++ spec/factories/employee.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :employee do
-    sequence(:email) { |n| "test#{n}@example.com" }
+    sequence(:email) { |n| "testy#{n}@example.com" }
     password { 'password123' }
   end
 end
