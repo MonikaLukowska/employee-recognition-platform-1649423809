0e096049f2c8a88a1333047ca5835d79e61cf232
MonikaLukowska <monika.lukowska@gmail.com>
2022-04-21 22:09:48 +0200

    Create admin users kudos controller

---
+++ app/controllers/adminUsers/kudos_controller.rb
@@ -0,0 +1,26 @@
+module AdminUsers
+  class KudosController < AdminUsers::ApplicationController
+    before_action :set_kudo, only: %i[destroy]
+
+    # GET /kudos
+    def index
+      @kudos = Kudo.all.includes(:receiver, :giver)
+    end
+
+    # DELETE /kudos/1
+    def destroy
+      @kudo.destroy
+      redirect_to kudos_path, notice: 'Kudo was successfully destroyed.'
+    end
+
+    private
+
+    def set_kudo
+      @kudo = Kudo.find(params[:id])
+    end
+
+    def kudo_params
+      params.require(:kudo).permit(:id, :title, :content, :receiver_id, :giver_id)
+    end
+  end
+end
