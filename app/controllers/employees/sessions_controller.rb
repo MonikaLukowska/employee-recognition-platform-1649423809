bc0181eff361120bd4636fac515bae1e1269449f
MonikaLukowska <monika.lukowska@gmail.com>
2022-04-21 11:14:43 +0200

    setup sign out paths for employee and admin

--- app/controllers/employees/sessions_controller.rb
+++ app/controllers/employees/sessions_controller.rb
@@ -1,29 +1,41 @@
 # frozen_string_literal: true
 
-class Employees::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-  include Accessible
-  skip_before_action :check_user, only: :destroy
+module Employees
+  class SessionsController < Devise::SessionsController
+    # before_action :configure_sign_in_params, only: [:create]
+    include Accessible
+    skip_before_action :check_user, only: :destroy
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+    # GET /resource/sign_in
+    # def new
+    #   super
+    # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    # POST /resource/sign_in
+    # def create
+    #   super
+    # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    # DELETE /resource/sign_out
+    def destroy
+      super
+    end
 
-  # protected
+    def after_sign_out_path_for(resource)
+      super(resource)
+      new_employee_session_path
+    end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # def after_sign_in_path_for(resource)
+    #   super(resource)
+    #   root_path
+    # end
+
+    # protected
+
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_in_params
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    # end
+  end
 end
