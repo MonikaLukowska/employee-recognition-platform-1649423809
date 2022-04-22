9f18ec82e0d18d317775519751277be3c4b5b1e0
MonikaLukowska <monika.lukowska@gmail.com>
2022-04-22 22:09:56 +0200

    Add login admin method

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -6,6 +6,13 @@ module LoginMacros
     click_button 'Log in'
   end
 
+  def login_admin(admin)
+    visit '/admin'
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Log in'
+  end
+
   def logout
     click_link 'Sign out'
   end
