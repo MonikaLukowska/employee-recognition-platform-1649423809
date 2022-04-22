7b3c9845a2a2cc90e3206a3eba074f71f302fde7
MonikaLukowska <monika.lukowska@gmail.com>
2022-04-22 22:08:04 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :kudos
+
+  root to: 'kudos#index'
+
+  devise_for :employees, path: '', controllers: {sessions: 'employees/sessions',passwords: 'employees/passwords',
+    registrations: 'employees/registrations'}
+
   devise_for :admin_users, path: 'admin', controllers: {sessions: 'admin_users/sessions', passwords: 'admin_users/passwords'}
 
   devise_scope :admin_user do
     get 'admin', to: 'admin_users/sessions#new'
   end
 
-  scope module: 'admin_users', path: 'admin' do
+  namespace :admin_users, path: 'admin' do
     get '/dashboard', to: '/admin_users/pages#dashboard', as: :dashboard
-    resources :kudos
+    resources :kudos, only: [:index, :destroy]
+    
   end
 
-  
-  resources :kudos
-
-  devise_for :employees, path: '', controllers: {sessions: 'employees/sessions',passwords: 'employees/passwords',
-    registrations: 'employees/registrations'}
-
-  root to: 'kudos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
