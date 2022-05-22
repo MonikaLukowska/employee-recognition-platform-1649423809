5754b1c806114cd04b68f35e9f6553055625b314
MonikaLukowska <monika.lukowska@gmail.com>
2022-05-23 01:13:31 +0200

    tests: add missing presence validation test, CR to PR #16

--- spec/models/company_value_spec.rb
+++ spec/models/company_value_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe CompanyValue, type: :model do
       create(:company_value)
     end
 
+    it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
   end
 end
