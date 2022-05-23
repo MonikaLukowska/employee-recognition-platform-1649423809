74b89f087febc6fff232f4cf12199689a9c9c5ba
MonikaLukowska <monika.lukowska@gmail.com>
2022-05-24 00:52:52 +0200

    fix: name typo

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,7 @@ class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver', dependent: :destroy, inverse_of: :receiver
-  has_many :gived_kudos, class_name: 'Kudo', foreign_key: 'giver', dependent: :destroy, inverse_of: :giver
+  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver', dependent: :destroy, inverse_of: :giver
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
