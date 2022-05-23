96bbdc7ee009cd1ff399a0e6cadc1bcf3af6b3c2
MonikaLukowska <monika.lukowska@gmail.com>
2022-05-24 00:51:17 +0200

    feat: add associations to Kudo and CompanyValue

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -1,3 +1,5 @@
 class CompanyValue < ApplicationRecord
+  has_many :kudos, foreign_key: 'company_value', inverse_of: :company_value, dependent: :destroy
+
   validates :title, uniqueness: { case_sensitive: false }, presence: true
 end
--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -1,6 +1,7 @@
 class Kudo < ApplicationRecord
   belongs_to :receiver, class_name: 'Employee', inverse_of: :received_kudos
-  belongs_to :giver, class_name: 'Employee', inverse_of: :gived_kudos
+  belongs_to :giver, class_name: 'Employee', inverse_of: :given_kudos
+  belongs_to :company_value, inverse_of: :kudos
 
   validates  :title, :content, presence: true
   validate :number_of_available_kudos_cannot_be_less_than_zero
