b8e754f6cc89106f2e58f3b853149ad18b522723
MonikaLukowska <monika.lukowska@gmail.com>
2022-05-30 13:49:48 +0200

    refactor: remove unnecessary options from kudo and company value models

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -1,5 +1,5 @@
 class CompanyValue < ApplicationRecord
-  has_many :kudos, foreign_key: 'company_value', inverse_of: :company_value, dependent: :destroy
+  has_many :kudos, dependent: :restrict_with_error
 
   validates :title, uniqueness: { case_sensitive: false }, presence: true
 end
