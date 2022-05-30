44b74734c36af50bd639747a921ea45f62074fe0
MonikaLukowska <monika.lukowska@gmail.com>
2022-05-30 22:12:37 +0200

    test: add Reward model spec

---
+++ spec/models/reward_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Reward, type: :model do
+  context 'when creating new company value' do
+    before do
+      create(:reward)
+    end
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
+  end
+end
