8f3bff99c06cc77e8e66872b2cef3d6f27728ce3
MonikaLukowska <monika.lukowska@gmail.com>
2022-06-09 23:26:45 +0200

    test: implement system spec for earned points

---
+++ spec/system/earned_points_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Earned points management', type: :system do
+  let(:employee) { create(:employee) }
+  let!(:kudo) { create(:kudo, receiver: employee) }
+
+  before do
+    login_employee(employee)
+    visit root_path
+  end
+
+  it 'shows number of earned points in navbar' do
+    within('nav.navbar') do
+      expect(page).to have_content(employee.earned_points)
+    end
+  end
+
+  it 'changes number of earned points after destroying kudo' do
+    expect(employee.earned_points).to eq 1
+    kudo.destroy
+    expect(employee.earned_points).to eq 0
+  end
+end
