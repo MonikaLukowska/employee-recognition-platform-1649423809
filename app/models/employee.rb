878534153067646e9e37d9ae8ac511408afed17a
MonikaLukowska <monika.lukowska@gmail.com>
2022-06-13 10:30:21 +0200

    feat: create Order model and adapt associated Employee and Reward models

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,11 +3,13 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver', dependent: :destroy, inverse_of: :receiver
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver', dependent: :destroy, inverse_of: :giver
+  has_many :orders, dependent: :nullify
+  has_many :rewards, through: :orders
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def earned_points
-    received_kudos.count
+    received_kudos.count - rewards.sum(:price).to_i
   end
 end
