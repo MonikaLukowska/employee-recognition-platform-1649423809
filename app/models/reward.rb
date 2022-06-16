a7a764c1dc04025cae1eea119848146a4c759364
MonikaLukowska <monika.lukowska@gmail.com>
2022-06-16 17:21:18 +0200

    refactor: CR to PR, remove unnecessary association

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,6 +1,6 @@
 class Reward < ApplicationRecord
   has_many :orders, dependent: :nullify
-  has_many :employees, through: :orders
+
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1 }
 end
