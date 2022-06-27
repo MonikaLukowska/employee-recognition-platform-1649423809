8465fbdafd3422b1b8099f2df4b3268dc11c1767
MonikaLukowska <monika.lukowska@gmail.com>
2022-06-27 15:20:05 +0200

    feat: add routes for employee's orders list in admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,11 @@ Rails.application.routes.draw do
   namespace :admin_users, path: 'admin' do
     get '/dashboard', to: '/admin_users/pages#dashboard', as: :dashboard
     resources :kudos, only: [:index, :destroy]
-    resources :employees, except: [:show]
+    resources :employees, except: [:show] do
+      scope module: 'employees' do
+        resources :orders, only: [:index]
+      end
+    end
     resources :company_values
     resources :rewards
   end
