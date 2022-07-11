02d5cec6bd325cfa1cb43fa108383e771ecad552
MonikaLukowska <monika.lukowska@gmail.com>
2022-07-11 11:35:50 +0200

    test: add order's filtering system test
    This also includes:
    - removing earned points number check, already implemented in earned_points spec
    - organizing spec to contexts
    - removing useless memoized helpers

--- spec/system/listing_orders_spec.rb
+++ spec/system/listing_orders_spec.rb
@@ -1,54 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe 'Listing orders management', type: :system do
-  let(:employee) { create(:employee) }
   let(:admin_user) { create(:admin_user) }
-  let!(:kudo) { create(:kudo, receiver: employee) }
-  let(:reward) { create(:reward) }
-  let!(:order) { create(:order, employee: employee, reward: reward) }
+  let!(:order) { create(:order) }
 
   before do
-    login_employee(employee)
+    login_employee(order.employee)
     visit orders_path
   end
 
-  it 'shows employee\'s orders list' do
-    within("li[test_id='order_#{order.id}") do
-      expect(page).to have_text(order.reward_snapshot.title)
-      expect(page).to have_text(order.reward_snapshot.price)
-      expect(page).to have_text(order.reward_snapshot.description)
-      expect(page).to have_text(order.created_at.strftime('%d-%m-%Y'))
+  context 'when listing orders' do
+    let(:order_delivered) { create(:order, employee: order.employee, status: 1) }
+
+    it 'shows employee\'s orders list' do
+      visit orders_path
+      within("li[test_id='order_#{order.id}") do
+        expect(page).to have_text(order.reward_snapshot.title)
+        expect(page).to have_text(order.reward_snapshot.price)
+        expect(page).to have_text(order.reward_snapshot.description)
+        expect(page).to have_text(order.created_at.strftime('%d-%m-%Y'))
+      end
+    end
+
+    it 'enables filtering orders' do
+      expect(page).to have_link('All orders')
+      expect(page).to have_link('Delivered')
+      expect(page).to have_link('Not delivered')
+      order_delivered
+
+      click_on 'Delivered'
+      expect(page).to have_css("li[test_id='order_#{order_delivered.id}")
+      expect(page).not_to have_css("li[test_id='order_#{order.id}")
+
+      click_on 'Not delivered'
+      expect(page).not_to have_css("li[test_id='order_#{order_delivered.id}")
+      expect(page).to have_css("li[test_id='order_#{order.id}")
+
+      click_on 'All orders'
+      expect(page).to have_css("li[test_id='order_#{order_delivered.id}")
+      expect(page).to have_css("li[test_id='order_#{order.id}")
     end
   end
 
-  it 'does not change order\'s reward price when admin edits reward\'s current price' do
-    within('nav.navbar') do
-      expect(page).to have_content('Earned points: 0')
-    end
-    using_session('Admin') do
-      login_admin(admin_user)
+  context 'when editing order price' do
+    it 'does not change order\'s reward price when admin edits reward\'s current price' do
+      using_session('Admin') do
+        login_admin(admin_user)
 
-      visit('/admin/rewards')
+        visit('/admin/rewards')
 
-      click_on 'Edit'
-      fill_in('Price', with: 2.0)
-      click_button('Update Reward')
-      reward.reload
-    end
+        click_on 'Edit'
+        fill_in('Price', with: 2.0)
+        click_button('Update Reward')
+        order.reward.reload
+      end
 
-    refresh
+      refresh
 
-    click_on 'Rewards'
-    within("li[test_id='reward_#{reward.id}") do
-      expect(page).to have_text('Price: 2.0')
-    end
+      click_on 'Rewards'
+      within("li[test_id='reward_#{order.reward.id}") do
+        expect(page).to have_text('Price: 2.0')
+      end
 
-    click_on 'Orders'
-    within('nav.navbar') do
-      expect(page).to have_content('Earned points: 0')
-    end
-    within("li[test_id='order_#{order.id}") do
-      expect(page).to have_text('Reward price: 1.0')
+      click_on 'Orders'
+      within("li[test_id='order_#{order.id}") do
+        expect(page).to have_text('Reward price: 1.0')
+      end
     end
   end
 end
