67a8cf0d94f8eec6185660cce307b0356b2916cf
MonikaLukowska <monika.lukowska@gmail.com>
2022-07-29 00:01:20 +0200

    feat: admin can increase number of kudos for all
    Implementation includes:
    - adding routes
    - adding add_kudos and increase_number_of_available_kudos methods to admin/employees controller
    - adding link to the update form in admin's panel
    - adding view with update form

--- app/controllers/adminUsers/employees_controller.rb
+++ app/controllers/adminUsers/employees_controller.rb
@@ -23,6 +23,27 @@ module AdminUsers
       redirect_to admin_users_employees_path, notice: 'Employee was successfully destroyed.'
     end
 
+    def add_kudos
+      render :add_kudos
+    end
+
+    def increase_number_of_available_kudos
+      number_of_kudos = params[:number_of_kudos].to_i
+      if number_of_kudos.between?(1, 20)
+        Employee.transaction do
+          Employee.all.each do |e|
+            e.increment(:number_of_available_kudos, number_of_kudos).save!
+          end
+          redirect_to admin_users_employees_path, notice: 'Numbers of available kudos where increased succesfully'
+        rescue StandardError => e
+          render :add_kudos, notice: "Update failed. Reason: #{e}"
+        end
+      else
+        render :add_kudos,
+               notice: 'Invalid input, please enter a number between 1 and 20'
+      end
+    end
+
     private
 
     def employee
