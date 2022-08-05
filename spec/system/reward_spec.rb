9daea1414d98aa7325ac3717651c8b50fcc31386
MonikaLukowska <monika.lukowska@gmail.com>
2022-08-05 13:58:36 +0200

    test: add pagination to rewards system spec

--- spec/system/reward_spec.rb
+++ spec/system/reward_spec.rb
@@ -26,4 +26,35 @@ RSpec.describe 'Reward management', type: :system do
     expect(page).to have_content(reward.description)
     expect(page).to have_content(reward.price)
   end
+
+  context 'when there are no more than 3 rewards' do
+    it 'there is no active next page link nor page 2 link' do
+      visit rewards_path
+      within('nav.pagy-bootstrap-nav') do
+        expect(page).to have_css('li.disabled', text: 'Next')
+        expect(page).not_to have_link('2')
+      end
+    end
+  end
+
+  context 'when there are more than 3 rewards' do
+    it 'shows 3 rewards per page only' do
+      rewards = create_list(:reward, 3)
+      visit rewards_path
+
+      expect(page).to have_content(rewards.last.title)
+      expect(page).to have_content(rewards.first.title)
+      expect(page).to have_content(rewards[1].title)
+      expect(page).not_to have_content(reward.title)
+      within('nav.pagy-bootstrap-nav') do
+        expect(page).to have_link('2')
+      end
+
+      click_link('2')
+      expect(page).to have_current_path('/rewards?page=2')
+      expect(page).to have_content(reward.title)
+      click_link('1')
+      expect(page).to have_current_path('/rewards?page=1')
+    end
+  end
 end
